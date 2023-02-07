bdc0a90dea5a8ef9eacc286504eb0d0f51194694
krypton1101 <krypton11@mail.ru>
2023-02-07 16:56:46 +0300

    Parallax_0.1
    Added two layer pictures(layer1.png, layer2.png)
    Added JavaScript file reading scroll value(CardGame.js)
    Added CSS file moving divs(CardGame.css)
    
    Problems found:
    - white background on article content and footer

--- php/CardGame.php
+++ php/CardGame.php
@@ -1,4 +1,3 @@
-<?php session_start();?>
 <!DOCTYPE html>
 <html lang="ru">
 <head>
@@ -7,15 +6,39 @@
         $pageСss ="main.css";
     ?>
     <?php require_once "../blocks/head.php"?>
-</head>
-<body class="bg-black">
-<?php
-$name = basename(__FILE__);
-require_once  "../blocks/header.php"
-?>
-<?php require_once "../blocks/footer.php"?>
 
-<script src="../node_modules/bootstrap/dist/js/bootstrap.bundle.js"></script>
-<script src="../node_modules/@popperjs/core/dist/umd/popper.min.js"></script>
+    <link href="../css/CardGame.css" rel="stylesheet">
+    <script src="../js/CardGame.js" defer></script>
+
+    <script src="../node_modules/bootstrap/dist/js/bootstrap.bundle.js" defer></script>
+    <script src="../node_modules/@popperjs/core/dist/umd/popper.min.js" defer></script>
+</head>
+<body>
+    <?php
+        $name = basename(__FILE__);
+        require_once  "../blocks/header.php"
+    ?>
+
+    <div class="layers">
+        <div class="layer layers-base" style="background-image: url(../images/layer2.png);"></div>
+        <div class="layer layers-front" style="background-image: url(../images/layer1.png);"></div>
+    </div>
+
+    <article>
+        <div class="container">
+            <div class="text-bg-dark rounded-5 p-3 my-5">
+                <h3>Lorem ipsum e.t.c.</h3>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Dictum non consectetur a erat. Consequat nisl vel pretium lectus quam id leo in. Mauris pharetra et ultrices neque. Et ultrices neque ornare aenean euismod. Quam vulputate dignissim suspendisse in est ante in nibh mauris. Risus viverra adipiscing at in tellus integer feugiat scelerisque varius. Blandit massa enim nec dui nunc mattis. Pharetra convallis posuere morbi leo. A diam sollicitudin tempor id eu nisl nunc mi ipsum. At volutpat diam ut venenatis. Turpis cursus in hac habitasse platea dictumst quisque sagittis. Egestas sed sed risus pretium quam vulputate dignissim. In egestas erat imperdiet sed euismod nisi porta. Sagittis aliquam malesuada bibendum arcu vitae.
+                    Id venenatis a condimentum vitae sapien pellentesque habitant morbi tristique. Consequat nisl vel pretium lectus quam id. Blandit libero volutpat sed cras. Ullamcorper a lacus vestibulum sed arcu non odio euismod. Maecenas accumsan lacus vel facilisis volutpat est velit egestas. Iaculis urna id volutpat lacus. Lectus nulla at volutpat diam ut venenatis tellus. Eu augue ut lectus arcu. Aenean sed adipiscing diam donec adipiscing tristique risus nec feugiat. Quis enim lobortis scelerisque fermentum dui faucibus in ornare. Volutpat diam ut venenatis tellus in metus vulputate. Nam aliquam sem et tortor consequat id porta nibh. Turpis massa tincidunt dui ut ornare lectus sit amet. Pellentesque habitant morbi tristique senectus. In tellus integer feugiat scelerisque varius morbi enim nunc. Diam quis enim lobortis scelerisque fermentum dui faucibus. Lectus quam id leo in vitae turpis massa.
+                    Commodo odio aenean sed adipiscing diam donec adipiscing tristique. Sed vulputate mi sit amet. Fermentum posuere urna nec tincidunt praesent semper feugiat. At consectetur lorem donec massa sapien faucibus. Eget nulla facilisi etiam dignissim diam. Varius vel pharetra vel turpis nunc eget lorem dolor. Pulvinar mattis nunc sed blandit. Suspendisse interdum consectetur libero id faucibus nisl tincidunt. Est velit egestas dui id ornare arcu odio. Dolor morbi non arcu risus quis varius quam quisque. Sit amet nulla facilisi morbi tempus iaculis urna id volutpat. Dolor sed viverra ipsum nunc aliquet bibendum enim. Nec tincidunt praesent semper feugiat nibh sed pulvinar proin. Sit amet volutpat consequat mauris nunc. Nunc consequat interdum varius sit amet mattis vulputate. Porttitor eget dolor morbi non arcu. Tellus rutrum tellus pellentesque eu tincidunt tortor aliquam nulla facilisi. In metus vulputate eu scelerisque. Scelerisque in dictum non consectetur a. Elementum eu facilisis sed odio.
+                    Ornare suspendisse sed nisi lacus sed viverra tellus. Pretium nibh ipsum consequat nisl vel pretium. Imperdiet nulla malesuada pellentesque elit. Nullam ac tortor vitae purus faucibus ornare. Sagittis vitae et leo duis ut diam quam nulla porttitor. Tincidunt ornare massa eget egestas purus viverra accumsan. Sagittis aliquam malesuada bibendum arcu vitae. Lorem mollis aliquam ut porttitor leo a. Sit amet consectetur adipiscing elit. Massa eget egestas purus viverra accumsan in nisl. Pulvinar elementum integer enim neque volutpat ac. Tellus orci ac auctor augue mauris augue.
+                    In mollis nunc sed id semper risus in hendrerit. Odio pellentesque diam volutpat commodo sed egestas egestas fringilla phasellus. Non nisi est sit amet facilisis magna etiam tempor orci. Diam vulputate ut pharetra sit amet aliquam id diam. Eu turpis egestas pretium aenean. Tempor commodo ullamcorper a lacus vestibulum. Lacus sed viverra tellus in hac habitasse platea. Eget lorem dolor sed viverra ipsum nunc aliquet. Leo integer malesuada nunc vel risus commodo viverra. Mi sit amet mauris commodo quis imperdiet massa tincidunt nunc. Amet facilisis magna etiam tempor orci eu. Et malesuada fames ac turpis egestas integer eget. Aliquam nulla facilisi cras fermentum odio eu. Consectetur purus ut faucibus pulvinar elementum integer enim. Tempor commodo ullamcorper a lacus vestibulum sed. Facilisis volutpat est velit egestas dui id ornare arcu odio. Ligula ullamcorper malesuada proin libero nunc.</p>
+            </div>
+        </div>
+    </article>
+
+    <?php require_once "../blocks/footer.php"?>
+
 </body>
 </html>
